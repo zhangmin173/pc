928e28565d924c29ccccb74da953954bd40473ea
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-06-06 09:03:00 +0800

    增加富文本编辑器 wangEditor

--- mock/category/all.js
+++ mock/category/all.js
@@ -5,21 +5,21 @@ function mock() {
             {
                 id: 1,
                 paraent_id: 0,
-                category_title: '大类',
+                category_title: '大类1',
                 category_icon: '',
                 create_time: '2018-06-10'
             },
             {
                 id: 1,
                 paraent_id: 0,
-                category_title: '大类',
+                category_title: '大类2',
                 category_icon: '',
                 create_time: '2018-06-10'
             },
             {
                 id: 1,
                 paraent_id: 0,
-                category_title: '大类',
+                category_title: '大类3',
                 category_icon: '',
                 create_time: '2018-06-10'
             }
--- src/pages/admin/article/list/index.js
+++ src/pages/admin/article/list/index.js
@@ -2,7 +2,7 @@
  * @Author: Zhang Min 
  * @Date: 2018-06-01 08:35:53 
  * @Last Modified by: Zhang Min
- * @Last Modified time: 2018-06-05 09:02:14
+ * @Last Modified time: 2018-06-06 09:01:16
  */
 
 import Toolkit from '../../../../components/toolkit';
@@ -47,13 +47,20 @@ $(function () {
                     this.edit(row, obj);
                 }
             });
+
+            this.getcategory({
+                paraent_id: 0
+            }, data => {
+                this.categoryParaent = data;
+            })
         }
         detail(row) {
             window.open('../detail/index.html?id=' + row.id);
         }
         edit(row, obj) {
+            row.categoryParaent = this.categoryParaent;
             const content = Template('tpl', row);
-            layer.open({
+            const layerIndex = layer.open({
                 type: 1,
                 title: '编辑',
                 closeBtn: 0,
@@ -61,13 +68,35 @@ $(function () {
                 shadeClose: true,
                 content
             });
+            layui.form.render();
             layui.laydate.render({
                 elem: '#laydate' //指定元素
             });
+            const editor = this.initEditor();
+            editor.txt.html(row.content);
+
+            let category_parent_id = 0;
+            layui.form.on('select(category_parent_id)', data => {
+                category_parent_id = data.value;
+                this.getcategory({
+                    paraent_id: category_parent_id
+                }, data => {
+                    const htmlStr = Template('tpl1', { data });
+                    $('#category_id').html(htmlStr);
+                    layui.form.render();
+                })
+            });
+
             layui.form.on('submit(editForm)', form => {
-                console.log(form.field);
-                this.update(form.field, () => {
-                    obj.update(form.field);
+                const formData = form.field;
+                formData.content = editor.txt.html();
+                if (category_parent_id) {
+                    formData.category_parent_id = category_parent_id;
+                }
+                console.log(formData);
+                this.update(formData, () => {
+                    obj.update(formData);
+                    layer.close(layerIndex);
                 })
                 return false;
             });
@@ -97,6 +126,44 @@ $(function () {
                         cb && cb(res);
                     } else {
                         layer.msg(res.msg);
+                    }
+                }
+            })
+        }
+        initEditor(id = '#editor') {
+            const E = window.wangEditor;
+            const editor = new E(id);
+            editor.customConfig.menus = [
+                'head',  // 标题
+                'bold',  // 粗体
+                'fontSize',  // 字号
+                'italic',  // 斜体
+                'underline',  // 下划线
+                'strikeThrough',  // 删除线
+                'foreColor',  // 文字颜色
+                'backColor',  // 背景颜色
+                'link',  // 插入链接
+                'list',  // 列表
+                'justify',  // 对齐方式
+                'emoticon',  // 表情
+                'image',  // 插入图片
+                'table',  // 表格
+                'video',  // 插入视频
+                'code',  // 插入代码
+                'undo',  // 撤销
+                'redo'  // 重复
+            ]
+            editor.customConfig.uploadImgShowBase64 = true;
+            editor.create();
+            return editor;
+        }
+        getcategory(querys, cb) {
+            Toolkit.ajax({
+                url: '/Category/all',
+                data: querys,
+                success: res => {
+                    if (res.success) {
+                        cb && cb(res.data);
                     }
                 }
             })
